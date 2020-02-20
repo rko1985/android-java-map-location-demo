a815b29866ab30d218b5b3ecd116a65e239773e2
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-20 10:48:43 -1000

    asking for user mission to access GPS location

--- app/src/main/java/com/myappcompany/russell/mapandlocationdemo/MapsActivity.java
+++ app/src/main/java/com/myappcompany/russell/mapandlocationdemo/MapsActivity.java
@@ -1,11 +1,16 @@
 package com.myappcompany.russell.mapandlocationdemo;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -54,6 +59,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         };
+
+        if (Build.VERSION.SDK_INT < 23) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, locationListener);
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, locationListener);
+            }
+        }
+
+
     }
 
 
