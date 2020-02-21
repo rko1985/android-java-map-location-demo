a435f645d892991cf1afcb1a24df034ace1e0f33
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-21 09:27:11 -1000

    getting information about location

--- app/src/main/java/com/myappcompany/russell/mapandlocationdemo/MapsActivity.java
+++ app/src/main/java/com/myappcompany/russell/mapandlocationdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.myappcompany.russell.mapandlocationdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,6 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -78,6 +85,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if(listAddresses != null && listAddresses.size() > 0){
+                        String address = "";
+
+                        if(listAddresses.get(0).getThoroughfare() != null) {
+                            address += listAddresses.get(0).getThoroughfare() + " ";
+                        }
+
+                        if(listAddresses.get(0).getLocality() != null) {
+                            address += listAddresses.get(0).getLocality() + " ";
+                        }
+
+                        if(listAddresses.get(0).getPostalCode() != null) {
+                            address += listAddresses.get(0).getPostalCode() + " ";
+                        }
+
+                        if(listAddresses.get(0).getAdminArea() != null) {
+                            address += listAddresses.get(0).getAdminArea();
+                        }
+
+                        Toast.makeText(MapsActivity.this, address, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
